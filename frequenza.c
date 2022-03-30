c3a058c35e8e6f35a2446cf5343d10bb9d9d4bcd
Zeo <leonardo.basso02@gmail.com>
2022-03-30 09:36:13 +0200

    continuazione

--- frequenza.c
+++ frequenza.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #define LUNGHEZZA_MASSIMA 255
-
+#define OFFSET_NUMERI 48
 
 void conta_frequenza(char *stringa, char *f, int lunghezza);
 
 int main() {
     char stringa_input[LUNGHEZZA_MASSIMA];
     char frequenza[10];
-    
+    scanf("%255s", stringa_input);    
     // all'iesima riga aumento il contatore nella posizione i-esima
 
 }
@@ -16,7 +16,7 @@ void conta_frequenza(char *stringa, char *f, int lunghezza) {
     for (int i = 0; i < lunghezza; i++) { 
         // scrivere il codice per confrontare l'iesimo elemento di una stringa per poi salvarne la
         // frequenza all'interno dell'array frequenza
-        int indice_frequenza = stringa[i];
+        int indice_frequenza = stringa[i] - OFFSET_NUMERI;
         f[indice_frequenza]+=1; 
     }
 }
