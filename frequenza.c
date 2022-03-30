15e0aac9b24f8109065367b223a7ab9d2104a3fc
Zeo <leonardo.basso02@gmail.com>
2022-03-30 09:04:47 +0200

    file aggiunto

---
+++ frequenza.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define LUNGHEZZA_MASSIMA 255
+
+
+void conta_frequenza(char *f, int lunghezza);
+
+int main() {
+    char stringa_input[LUNGHEZZA_MASSIMA];
+    char frequenza[10];
+    
+    // all'iesima riga aumento il contatore nella posizione i-esima
+
+}
+
+void conta_frequenza(char *f, int lunghezza) {
+    for (int i = 0; i < lunghezza; i++) { 
+        // scrivere il codice per confrontare l'iesimo elemento di una stringa per poi salvarne la
+        // frequenza all'interno dell'array frequenza
+    }
+}
